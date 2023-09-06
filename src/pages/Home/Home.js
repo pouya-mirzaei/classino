db952b850dd6895df63734f3bf7c10a58a899da1
pouya <pouya.hp2004@yahoo.com>
2023-09-06 21:04:50 +0330

    Upadate : added the hero section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import PreLoader from '../../components/PreLoader';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
   return (
     <>
       <Header></Header>
+      <HeroSection></HeroSection>
     </>
   );
 }
--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -37,64 +37,6 @@ export default function Header() {
 
   return (
     <>
-      <header
-        className={`flex items-center transition-all justify-between lg:justify-around flex-row-reverse lg:flex-row h-20 sticky top-0 px-3.5 ${
-          scrolled ? 'shadow-lg' : ''
-        }`}>
-        {/* right side */}
-
-        <div className="flex items-center gap-x-5">
-          {/* logo */}
-
-          <div>
-            <a href="/">
-              <img src="images/classinoLogo.08df55af.svg" alt="classino logo" />
-            </a>
-          </div>
-
-          {/* courses */}
-
-          <Courses></Courses>
-
-          {/* search bar */}
-
-          <div className="relative hidden lg:flex">
-            <input
-              type="text"
-              autoComplete="off"
-              placeholder="جست و جو دوره، پایه تحصیلی، استاد"
-              className="w-80 xl:w-[400px] rounded-[34px] outline-none py-2 px-5 bg-[#f0f0f1] shadow-lg placeholder:text-sm hover:bg-white focus:bg-white transition-all"
-            />
-            <svg className="h-full p-2 rounded-full bg-primary-2 cursor-pointer text-white absolute left-0">
-              <use href="sprite/hero.svg#magnifying-glass"></use>
-            </svg>
-          </div>
-        </div>
-
-        {/* left side */}
-
-        <div className="hidden lg:inline-block">
-          <button className="btn-panel">ورود به پنل کاربری</button>
-        </div>
-
-        {/* mobile icons */}
-        <div className="flex items-center gap-x-2.5 lg:hidden h-full [&>*]:w-10">
-          <svg
-            className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all"
-            onClick={toggleMobileMenu}>
-            <use href="sprite/hero.svg#3-bars"></use>
-          </svg>
-          <svg
-            className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all"
-            onClick={toggleMobileSearch}>
-            <use href="sprite/hero.svg#magnifying-glass"></use>
-          </svg>
-          <svg className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all">
-            <use href="sprite/hero.svg#user-circle"></use>
-          </svg>
-        </div>
-      </header>
-
       {/* assets */}
 
       <div>
@@ -160,6 +102,63 @@ export default function Header() {
           className={`absolute z-10 inset-0 bg-white/30 backdrop-blur-sm ${isMobileSearchOpen ? 'block' : 'hidden'}`}
           onClick={closeMobileSearch}></div>
       </div>
+      <header
+        className={`flex items-center bg-white transition-all justify-between lg:justify-around flex-row-reverse lg:flex-row h-20 sticky top-0 px-3.5 ${
+          scrolled ? 'shadow-lg' : ''
+        }`}>
+        {/* right side */}
+
+        <div className="flex items-center gap-x-5">
+          {/* logo */}
+
+          <div>
+            <a href="/">
+              <img src="images/classinoLogo.08df55af.svg" alt="classino logo" />
+            </a>
+          </div>
+
+          {/* courses */}
+
+          <Courses></Courses>
+
+          {/* search bar */}
+
+          <div className="relative hidden lg:flex">
+            <input
+              type="text"
+              autoComplete="off"
+              placeholder="جست و جو دوره، پایه تحصیلی، استاد"
+              className="w-80 xl:w-[400px] rounded-[34px] outline-none py-2 px-5 bg-[#f0f0f1] shadow-lg placeholder:text-sm hover:bg-white focus:bg-white transition-all"
+            />
+            <svg className="h-full p-2 rounded-full bg-primary-2 cursor-pointer text-white absolute left-0">
+              <use href="sprite/hero.svg#magnifying-glass"></use>
+            </svg>
+          </div>
+        </div>
+
+        {/* left side */}
+
+        <div className="hidden lg:inline-block">
+          <button className="btn-panel">ورود به پنل کاربری</button>
+        </div>
+
+        {/* mobile icons */}
+        <div className="flex items-center gap-x-2.5 lg:hidden h-full [&>*]:w-10">
+          <svg
+            className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all"
+            onClick={toggleMobileMenu}>
+            <use href="sprite/hero.svg#3-bars"></use>
+          </svg>
+          <svg
+            className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all"
+            onClick={toggleMobileSearch}>
+            <use href="sprite/hero.svg#magnifying-glass"></use>
+          </svg>
+          <svg className="hover:border-b hover:border-black active:translate-y-0.5 inline-block transition-all">
+            <use href="sprite/hero.svg#user-circle"></use>
+          </svg>
+        </div>
+      </header>
     </>
   );
 }
