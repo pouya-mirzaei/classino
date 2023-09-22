811cfe61d6d02d032b826f14694d464355251e4a
pouya <pouya.hp2004@yahoo.com>
2023-09-22 20:28:54 +0330

    implemented the dark mode for the calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,13 +6,13 @@ export default function Calendar() {
   return (
     <div className="basis-5/12 w-full shadow-md shadow-black/20 rounded-xl">
       {/* header */}
-      <div className="p-5 text-center bg-[#eb3b5a] border-b-4 border-dashed border-white rounded-t-xl">
-        <span className="text-white text-3xl font-semibold">{currentDate.month}</span>
+      <div className="p-5 text-center bg-[#eb3b5a] dark:bg-dark-1 border-b-4 border-dashed border-white rounded-t-xl">
+        <span className="text-white dark:text-[#eb3b5a] text-3xl font-semibold">{currentDate.month}</span>
       </div>
       {/* body */}
-      <div className="bg-white p-5 flex flex-col gap-8 justify-center text-center rounded-b-xl">
-        <span className="text-[#eb3b5a] text-6xl font-semibold">{currentDate.day}</span>
-        <span className="text-2xl font-medium">{currentDate.weekDay}</span>
+      <div className="bg-white dark:bg-dark-1 p-5 flex flex-col gap-8 justify-center text-center rounded-b-xl">
+        <span className="text-[#eb3b5a] dark:text-white text-6xl font-semibold">{currentDate.day}</span>
+        <span className="text-2xl font-medium dark:text-white">{currentDate.weekDay}</span>
       </div>
     </div>
   );
