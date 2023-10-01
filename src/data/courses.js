83ea02364be74ccde279c71cbb76e330591e4052
pouya <pouya.hp2004@yahoo.com>
2023-10-01 23:16:01 +0330

    added the courses data

---
+++ src/data/courses.js
@@ -0,0 +1,9 @@
+const courses = [
+  { id: 0, name: '', description: '', image: '', teacherId: 0, teacherImage: '' },
+  { id: 1, name: '', description: '', image: '', teacherId: 1, teacherImage: '' },
+  { id: 2, name: '', description: '', image: '', teacherId: 2, teacherImage: '' },
+  { id: 3, name: '', description: '', image: '', teacherId: 3, teacherImage: '' },
+  { id: 4, name: '', description: '', image: '', teacherId: 4, teacherImage: '' },
+  { id: 5, name: '', description: '', image: '', teacherId: 5, teacherImage: '' },
+];
+export default courses;
